5375ba5566fcd688dbfc072255927db923197aba
Andrewbors <63158102+Andrewbors@users.noreply.github.com>
2020-11-23 22:22:56 +0200

    Add files via upload

---
+++ WpfLMI/Abiturient.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfLMI
+{
+    public class Abiturient : INotifyPropertyChanged
+    {
+        private string zno;
+        private string study;
+        private string school;
+        public Abiturient(string zno = "", string study = "", string school = "")
+        {
+            this.zno = zno;
+            this.study = study;
+            this.school = school;
+        }
+        public Abiturient(Abiturient abiturient)
+        {
+            this.zno = abiturient.zno;
+            this.study = abiturient.study;
+            this.school = abiturient.school;
+        }
+        public Abiturient(string study, string school)
+        {
+            this.zno = "";
+            this.study = study;
+            this.school = school;
+        }
+        public Abiturient(string school)
+        {
+            this.zno = "";
+            this.study = "";
+            this.school = school;
+        }
+        public string Zno
+        {
+            get { return zno; }
+            set
+            {
+                zno = value;
+                OnPropertyChanged("Zno");
+            }
+        }
+        public string Study
+        {
+            get { return study; }
+            set
+            {
+                study = value;
+                OnPropertyChanged("Study");
+            }
+        }
+        public string School
+        {
+            get { return school; }
+            set
+            {
+                school = value;
+                OnPropertyChanged("School");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfLMI/ApplicationViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace WpfLMI
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Abiturient selectedAbiturient;
+        public ObservableCollection<Abiturient> Abiturient { get; set; }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Abiturient abiturient = new Abiturient();
+                      Abiturient.Insert(0, abiturient);
+                      SelectedAbiturient = abiturient;
+                  }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Abiturient abiturient = obj as Abiturient;
+                      if (abiturient != null)
+                      {
+                          Abiturient.Remove(abiturient);
+                      }
+                  },
+                 (obj) => Abiturient.Count > 0));
+            }
+        }
+
+        public Abiturient SelectedAbiturient
+        {
+            get { return selectedAbiturient; }
+            set
+            {
+                selectedAbiturient = value;
+                OnPropertyChanged("SelectedAbiturient");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Abiturient = new ObservableCollection<Abiturient>
+            {
+                new Abiturient { Zno="152", Study="10.2", School="4 school"},
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
